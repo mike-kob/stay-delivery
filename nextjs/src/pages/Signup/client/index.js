69c6b6030d16640b69bf183fe854e9f6c6b9aa93
AngryKat <gatto_candido@ukr.net>
2021-03-01 17:09:59 +0200

    add css to sign-up and log-in pages

--- nextjs/src/pages/Signup/client/index.js
+++ nextjs/src/pages/Signup/client/index.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
 import {
-  Button,
-  Form,
+  Button, Container,
+  Form, Col, Row, Tabs, Tab, Nav,
 } from 'react-bootstrap';
 import {useRouter} from 'next/router';
 
 import styles from './ClientSignup.module.css';
 import {clientSignupWithEmail} from '@/auth/actions';
+import FormsHeader from '@/components/FormsHeader';
 
 const ClientSignup = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [key, setKey] = useState('signup');
 
   const onSignup = async () => {
     console.log('STARTED');
@@ -21,34 +23,46 @@ const ClientSignup = () => {
   };
 
   return (
-    <div style={{width: '50%', border: 'solid 1px'}}>
-      Signup client with password
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Enter email"
-        />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container>
+      <FormsHeader/>
+      <div className={styles.main}>
+        <div className="row justify-content-center">
+          <Col xs={6}>
+            <div className="row justify-content-around">
+              <div className={styles.main_text}>Sign Up</div>
+              <a href="/login" className={styles.muted_main_text}>Log In</a>
+            </div>
+            <Form className={styles.form_area}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="E-mail"
+                  className={styles.form_input}
+                />
+              </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Group>
-      <Button variant="primary" onClick={onSignup}>
-        Signup
-      </Button>
-    </div>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                  className={styles.form_input}
+
+                />
+              </Form.Group>
+            </Form>
+            <Button className={styles.orng_button} onClick={onSignup}>
+            Sign up
+            </Button>
+          </Col>
+        </div>
+
+      </div>
+    </Container>
+
   );
 };
 
