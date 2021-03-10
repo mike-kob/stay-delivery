53ee35d343913f2cbc9dd420f0637715c980ef89
AngryKat <gatto_candido@ukr.net>
2021-03-10 23:09:51 +0200

    Add functionality to orders page

--- nextjs/src/pages/admin/Orders/index.js
+++ nextjs/src/pages/admin/Orders/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 import styles from './Orders.module.css';
 
+import OrdersContainer from "@/pages/admin/OrdersContainer";
+
 const Orders = () => {
   return (
     <div className={styles.main}>
-      This is orders page
-    </div>
+        <div className="row justify-content-center">
+                <a href="account" className={styles.muted_nav_item}>
+                    Information
+                </a>
+                <a href="orders" className={styles.selected_nav_item}>
+                    Orders
+                </a>
+                <a href="menu" className={styles.muted_nav_item}>
+                    Menu
+                </a>
+        </div>
+        <OrdersContainer/>
+        </div>
   );
 };
 
