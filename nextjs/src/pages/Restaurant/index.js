516ce5dd4b5876ac2632e095655974327c199f80
Masha <marvisnotamused@gmail.com>
2021-03-12 09:05:20 +0200

    init commit

--- nextjs/src/pages/Restaurant/index.js
+++ nextjs/src/pages/Restaurant/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useRouter} from 'next/router';
+import {clientGraphql} from '@/graphql';
+import {GET_RESTAURANT_QUERY} from '@/graphql/restaurant';
+// import ItemsGrid from '@/components/ItemsGrid';
 
 import styles from './Restaurant.module.css';
 
 const Restaurant = () => {
   const router = useRouter();
+  const [restaurant, setRestaurant] = useState({ dishes: [] });
+
+  useEffect(() => {
+    (async function() {
+      if (router.query.id) {
+        const {data} = await clientGraphql(GET_RESTAURANT_QUERY, {id: router.query.id});
+        setRestaurant(data?.restaurant);
+      }
+    })();
+  }, [router.query.id]);
 
   return (
     <div className={styles.main}>
-      This is page of restaurant with id {router.query.id}
+      <img src={restaurant.photo}></img>
+      <h1 className={styles.mainHeader}>{restaurant.name}</h1>
+      <div>{restaurant.description}</div>
+      <h2>Dishes</h2>
+      <div>
+      {
+        restaurant.dishes.map((item) => (
+          <Item key={item.key} item={item} />))
+      }
+      </div>
+    </div>
+  );
+};
+
+const Item = ({item}) => {
+  return (
+    <div className={styles.main}>
+      <h3>{item.name}</h3>
+      <p>{item.description}</p>
+      <p>{item.weight}</p>
+      <p>{item.price}</p>
+      <img src={item.photo}></img>
     </div>
   );
 };
